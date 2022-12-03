f9d087fd73dce7232f9f8a43b5749a78882b3704
IslamelGebaly <islamlang@gmail.com>
2022-12-03 21:27:27 +0200

    _

--- WordNet.java
+++ WordNet.java
@@ -1,25 +1,33 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class WordNet {
 
     // constructor takes the name of the two input files
     private Digraph G;
-    private ST<String, Integer> synsets;
+    private ST<String, Integer> nouns;
+    private ArrayList<String> synsets;
 
     private boolean isRooted() {
         boolean rootFound = false;
         for (int v = 0; v < G.V(); v++) {
-            if (G.outdegree(v) == 0 && !rootFound) {
-                rootFound = true;
-            } else if (rootFound) {
-                return false;
+            if (G.outdegree(v) == 0) {
+                if (rootFound)
+                    return false;
+                else
+                    rootFound = true;
             }
         }
         return rootFound;
+    }
+
+    private String getSynset(int id) {
+        return this.synsets.get(id);
     }
 
     public WordNet(String synsets, String hypernyms) {
@@ -31,13 +39,16 @@ public class WordNet {
 
         In synsetFile = new In(synsets);
         In hypernymFile = new In(hypernyms);
-        this.synsets = new ST<String, Integer>();
+        this.nouns = new ST<String, Integer>();
+        this.synsets = new ArrayList<String>();
         String s;
+
         int i = 0;
         for (String line : synsetFile.readAllLines()) {
             s = Arrays.stream(line.split(",")).toArray()[1].toString();
+            this.synsets.add(s);
             for (String word : s.split(" "))
-                this.synsets.put(word, i);
+                this.nouns.put(word, i);
             i++;
         }
 
@@ -47,7 +58,7 @@ public class WordNet {
         for (String line : hypernymFile.readAllLines()) {
             temp = line.split(",");
             v = Integer.parseInt(temp[0]);
-            for (i = 0; i < temp.length; i++) {
+            for (i = 1; i < temp.length; i++) {
                 w = Integer.parseInt(temp[i]);
                 G.addEdge(v, w);
             }
@@ -59,20 +70,21 @@ public class WordNet {
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return this.synsets.keys();
+        return this.nouns.keys();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
         if (word == null)
             throw new IllegalArgumentException();
-        return synsets.get(word) == null ? false : true;
+        return this.nouns.get(word) == null ? false : true;
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
         if (!this.isNoun(nounA) || !this.isNoun(nounB))
             throw new IllegalArgumentException();
+
 
         return 0;
     }
@@ -83,12 +95,14 @@ public class WordNet {
         if (!this.isNoun(nounA) || !this.isNoun(nounB))
             throw new IllegalArgumentException();
 
+
         return "";
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet wn = new WordNet("synsets.txt", "hypernyms.txt");
-        wn.sap(null, "ns");
+
+        StdOut.println(wn.getSynset(83));
     }
 }
